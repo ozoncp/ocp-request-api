6a8f7c3e4ea2fb6f7a29fcb2eec4a449ab2814e1
Maxim Vershinin <vershinin.m.s@gmail.com>
2021-07-24 23:49:47 +0400

    task-3: add benchmark test for SplitToBulks

--- internal/utils/requests_test.go
+++ internal/utils/requests_test.go
@@ -53,3 +53,21 @@ func TestSliceOfRequestsToMap(t *testing.T) {
 	assert.Equal(t, 0, len(byId))
 	assert.Nil(t, err)
 }
+
+func BenchmarkSplitToBulks(b *testing.B) {
+	reqNum := 10000000
+	requests := make([]models.Request, 0, reqNum)
+
+	for i := uint64(0); i < uint64(reqNum); i++ {
+		requests = append(requests, models.NewRequest(i, i, i, "test request"))
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		SplitToBulks(requests, 1)
+		SplitToBulks(requests, 10)
+		SplitToBulks(requests, 100)
+		SplitToBulks(requests, 1000)
+		SplitToBulks(requests, 10000)
+	}
+}
