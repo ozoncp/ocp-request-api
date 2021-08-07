3edaf9aba457fdc9c1e292ddb204f59d9821760e
Maxim Vershinin <vershinin.m.s@gmail.com>
2021-08-07 22:55:21 +0400

    task-5: introduce and implement Saver interface (including its tests)

---
+++ internal/saver/saver.go
@@ -0,0 +1,131 @@
+package saver
+
+import (
+	"github.com/ozoncp/ocp-request-api/internal/flusher"
+	"github.com/ozoncp/ocp-request-api/internal/models"
+	"log"
+	"sync"
+	"time"
+)
+
+const (
+	inited = iota + 1
+	closed
+)
+
+// Saver instance saves Request into underlying storage.
+// User must call Init() before using an instance.
+// And Close() to ensure all pending item are stored.
+// Closed instance cannot be used.
+type Saver interface {
+	Save(entity models.Request)
+	Init()
+	Close()
+}
+
+// NewSaver creates a new Saver instance.
+// It asynchronously collects save Requests into internally slice with given `capacity`.
+// It flushes Requests into underlying `flusher` with `flushEvery` periodicity.
+func NewSaver(capacity uint, flusher flusher.Flusher, flushEvery time.Duration) Saver {
+	s := &saver{
+		capacity:   capacity,
+		flusher:    flusher,
+		flushQueue: make(chan models.Request, capacity),
+		wait:       &sync.WaitGroup{},
+		flushEvery: flushEvery,
+	}
+	s.Init()
+	return s
+}
+
+type saver struct {
+	capacity   uint
+	flusher    flusher.Flusher
+	flushQueue chan models.Request
+	wait       *sync.WaitGroup
+	state      int8 // to check if it's closed or inited
+	flushEvery time.Duration
+}
+
+// Save Request into underlying storage
+func (s *saver) Save(request models.Request) {
+	s.mustNotBeClosed()
+	s.mustBeInitialized()
+
+	s.flushQueue <- request
+}
+
+// Init initiates saver so it's ready to Save Requests
+func (s *saver) Init() {
+	s.mustNotBeClosed()
+	if s.isInited() {
+		return
+	}
+
+	ticker := time.NewTicker(s.flushEvery)
+	s.wait.Add(1)
+
+	go func() {
+		requests := make([]models.Request, 0, s.capacity)
+		for {
+			select {
+			case req, ok := <-s.flushQueue:
+				if !ok {
+					requests = s.flush(requests)
+					s.wait.Done()
+					return
+				} else {
+					requests = append(requests, req)
+				}
+			case <-ticker.C:
+				requests = s.flush(requests)
+			}
+		}
+
+	}()
+	s.state |= inited
+}
+
+// Close ensures all Requests object are processed. Closed Saver does not accept new saves.
+func (s *saver) Close() {
+	s.mustBeInitialized()
+	if s.isClosed() {
+		return
+	}
+
+	s.state |= closed
+	close(s.flushQueue)
+	s.wait.Wait()
+}
+
+// flushes a slice of Requests and makes a new empty slice with a certain capacity
+func (s *saver) flush(requests []models.Request) []models.Request {
+	if len(requests) == 0 {
+		return requests
+	}
+
+	if failedToFlushReq, err := s.flusher.Flush(requests); err != nil {
+		log.Printf("failed to save %v requests: %v", len(failedToFlushReq), err)
+	}
+	return make([]models.Request, 0, s.capacity)
+}
+
+func (s *saver) mustNotBeClosed() {
+	if s.isClosed() {
+		panic("Saver instance is closed")
+	}
+}
+
+func (s *saver) mustBeInitialized() {
+	if !s.isInited() {
+		panic("Saver instance is not Init()-ed")
+	}
+}
+
+func (s *saver) isClosed() bool {
+	return s.state&closed == closed
+}
+
+func (s *saver) isInited() bool {
+	return s.state&inited == inited
+}
